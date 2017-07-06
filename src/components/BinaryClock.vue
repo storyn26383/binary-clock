01fd6d182019b20df872bffa4abff729fde08f63
Sasaya <storyn26383@gmail.com>
2017-07-07 01:52:52 +0800

    true binary mode

--- src/components/BinaryClock.vue
+++ src/components/BinaryClock.vue
@@ -1,19 +1,30 @@
 <template lang="pug">
   .binary-clock
-    binary-digit(:number="hours")
-    binary-digit(:number="minutes")
-    binary-digit(:number="seconds")
+    .switch(@click="trueMode = !trueMode")
+      light(:on="trueMode")
+      | True Binary Mode
+    .bcd-mode(v-if="!trueMode")
+      binary-digit(:number="hours")
+      binary-digit(:number="minutes")
+      binary-digit(:number="seconds")
+    .true-mode(v-if="trueMode")
+      true-binary-digit(:number="hours")
+      true-binary-digit(:number="minutes")
+      true-binary-digit(:number="seconds")
 </template>
 
 <script>
+  import Light from './Light'
   import BinaryDigit from './BinaryDigit'
+  import TrueBinaryDigit from './TrueBinaryDigit'
 
   export default {
     components: {
-      BinaryDigit
+      Light, BinaryDigit, TrueBinaryDigit
     },
     data () {
       return {
+        trueMode: false,
         hours: 0,
         minutes: 0,
         seconds: 0
@@ -33,11 +44,42 @@
 
 <style lang="scss" scoped>
   .binary-clock {
-    display: flex;
-    flex-direction: row;
+    .switch {
+      margin-left: 8px;
+      font-family: monospace;
+      color: #9ac9c4;
+      font-size: 20px;
+      display: flex;
+      align-items: center;
+      cursor: pointer;
 
-    .binary-digit {
-      margin: 0 8px;
+      .light {
+        width: 35px;
+        height: 35px;
+        margin-right: 16px;
+
+        &.on {
+          background-color: #9ac9c4;
+        }
+      }
+    }
+
+    .bcd-mode {
+      display: flex;
+      flex-direction: row;
+
+      .binary-digit {
+        margin: 0 8px;
+      }
+    }
+
+    .true-mode {
+      display: flex;
+      flex-direction: column;
+
+      .true-binary-digit {
+        margin: 8px;
+      }
     }
   }
 </style>
---
+++ src/components/TrueBinaryDigit.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  .true-binary-digit
+    light(:on="!!(number & 32)")
+    light(:on="!!(number & 16)")
+    light(:on="!!(number & 8)")
+    light(:on="!!(number & 4)")
+    light(:on="!!(number & 2)")
+    light(:on="!!(number & 1)")
+</template>
+
+<script>
+  import Light from './Light'
+
+  export default {
+    components: {
+      Light
+    },
+    props: {
+      number: {
+        type: Number,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .binary-digit {
+    display: flex;
+    flex-direction: row;
+  }
+</style>
